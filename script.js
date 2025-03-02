1c10ad07122be854fa568a18d958ea2f1f1d0ed8
MinatoDevel <sgminato69@gmail.com>
2025-03-02 23:36:32 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+// Add interactivity here
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Thank you for reaching out! I will get back to you soon.');
+  // You can add code here to send the form data to a server
+});
